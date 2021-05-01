4059d06f21a7409ec399a31000eb321556a951b0
Sara Bertuldo <sarabertuldo@gmail.com>
2021-05-01 18:08:03 -0500

    sets up scaffolding, dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Equipment from "./components/Equipment";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavLink className="link" to="/Home">
+          Home
+        </NavLink>
+        <NavLink className="link" to="/About">
+          About
+        </NavLink>
+        <NavLink className="link" to="/Equipment">
+          Equipment
+        </NavLink>
+        <NavLink className="link" to="/Contact">
+          Contact
+        </NavLink>
+
+        <main>
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/equipment" component={Equipment} />
+            <Route path="/contact" component={Contact} />
+            <Route path="*">
+              <Redirect to="/home" />
+            </Route>
+          </Switch>
+          SSSSSSSSsalt n pepa's here
+        </main>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Equipment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Equipment = () => {
+  return (
+    <>
+      <h1>Equipment Page!</h1>
+    </>
+  );
+};
+
+export default Equipment;
