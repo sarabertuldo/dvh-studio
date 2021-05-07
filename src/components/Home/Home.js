3ed17e1a0cacbd5f5b80fcffcc9d76b9f25768da
Sara Bertuldo <sarabertuldo@gmail.com>
2021-05-06 19:35:59 -0500

    updates equipment page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
       <h1>Welcome</h1>
+
+      <div className="welcome-container">
+        <img src="https://i.ibb.co/Qp087jk/dh-drums.jpg" alt="Divine Hammer" />
+      </div>
     </>
   );
 };
